e2a97c3841670f6e2055d3a368638d47ce33de67
pepm17 <33205049+pepm17@users.noreply.github.com>
2019-11-02 16:33:59 -0500

    Se creó el componente camera
    Este componente sirve para establecer la cámara

--- src/component/camera.js
+++ src/component/camera.js
@@ -1,37 +1,26 @@
-import React from "react";
-
-export default class Camera extends React.Component {
+export async function camera() {
     videoRef = React.createRef();
-
-    componentDidMount() {
-        if (navigator.mediaDevices.getUserMedia) {
-          //Define una promesa que será usada para cargar la camara y leer los frame
-          const webcamPromise = navigator.mediaDevices.getUserMedia({
-              video: true,
-              audio: false,
-            }).then(stream => {
-                // Pasar el actual frame a window.stream
-                window.stream = stream;
-                // Pasar el stream a videoRef
-                this.videoRef.current.srcObject = stream;
-                return new Promise(resolve => {
-                  this.videoRef.current.onloadedmetadata = () => {
-                    resolve();
-                  };
-                });
-            }, (error) => {
-                console.log("No se pudo iniciar la camara")
-                console.error(error)
+    if (navigator.mediaDevices.getUserMedia) {
+      //Define una promesa que será usada para cargar la camara y leer los frame
+      const webcamPromise = navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: false,
+        }).then(stream => {
+            // Pasar el actual frame a window.stream
+            window.stream = stream;
+            // Pasar el stream a videoRef
+            this.videoRef.current.srcObject = stream;
+            return new Promise(resolve => {
+              this.videoRef.current.onloadedmetadata = () => {
+                resolve();
+              };
             });
-    
-          // Define una promesa que pueda cargar el modelo
-          const loadlModelPromise = cocoSsd.load({base: "mobilenet_v2"});      
-          // Todas las promesas
-          Promise.all([loadlModelPromise, webcamPromise]).then(values => {
-              this.detectFromVideoFrame(values[0], this.videoRef.current);
-            }).catch(error => {
-              console.error(error);
-            });
-        }
-      }
+        }, (error) => {
+            console.log("No se pudo iniciar la camara")
+            console.error(error)
+        });
+    }
+  return webcamPromise;          
 }
+
+export default camera;
